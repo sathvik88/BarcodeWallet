397b92043f1c09012ef76c01f468c47cee206f7b
sathvik88 <sathvikkonuganti@gmail.com>
2024-11-19 16:30:52 -0500

    Initial Commit

---
+++ BarcodeWallet/BarcodeWalletApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeWalletApp.swift
+//  BarcodeWallet
+//
+//  Created by Sathvik Konuganti on 11/19/24.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeWalletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
