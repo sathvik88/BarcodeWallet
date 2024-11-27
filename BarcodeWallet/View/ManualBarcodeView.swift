13e8db0d24080de77912aefc44e4a7658693389a
sathvik88 <sathvikkonuganti@gmail.com>
2024-11-26 21:23:48 -0500

    Fixed issue with scanning barcode in dark mode.

--- BarcodeWallet/View/ManualBarcodeView.swift
+++ BarcodeWallet/View/ManualBarcodeView.swift
@@ -9,29 +9,20 @@ import SwiftUI
 
 struct ManualBarcodeView: View {
     @Binding var inputText: String
-    var barcodeGen = BarcodeGenModel()
     var body: some View {
         VStack{
-//            Text("Please key in your barcode data in the text field")
-//                    .font(.headline)
-//                    .padding(.bottom, 20)
-//            TextField("", text: $inputText)
-//                    .padding()
-//                    .font(.title)
-//                    .background(Color(.systemGray6))
-//                    .clipShape(RoundedRectangle(cornerSize: CGSize(width: 10, height: 10)))
-//            Spacer()
-//
-//                VStack(spacing: 0) {
-//                    barcodeGen.generateBarcode(text: inputText)
-//                        .resizable()
-//                        .scaledToFit()
-//
-//                    Text(inputText.isEmpty ? "Unknown data" : inputText)
-//                }
-            CodabarBarcodeView(codabarString: "A25149005991464B")
+            ZStack{
+                Rectangle()
+                    .frame(maxWidth: .infinity)
+                    .frame(maxHeight: 250)
+                    .foregroundStyle(Color.white)
+                CodabarView(text: .constant("A25149005991464B"))
+                    .frame(height: 100)
+                    .padding()
+            }
+            
         }
-        .padding()
+        
     }
 }
 
